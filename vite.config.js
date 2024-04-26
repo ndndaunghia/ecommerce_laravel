56f89ccbef6fe7e28342c58fc75e4aef91029304
unknown <ndndaunghia@gmail.com>
2024-04-26 14:16:03 +0700

    fix config vite

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // 'resources/sass/app.scss',
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
